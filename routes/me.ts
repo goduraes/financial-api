2ae99b27c49c1a4af4e87ba418708af564cdab0f
Gabriel Oliveira Duraes <drt.gduraes@ifood3rd.com>
2026-04-15 16:16:16 -0300

    add me/edit

--- routes/me.ts
+++ routes/me.ts
@@ -20,5 +20,20 @@ router.get('/', authMiddleware(), async (req: Request, res: Response) => {
     }
 });
 
+router.put('/edit', authMiddleware(), async (req: Request, res: Response) => {
+    const result = extractBearerToken(req.headers.authorization);
+    if ("error" in result) return res.status(401).json({ error: result.error });
+
+    const { name, email } = req.body;
+    
+    try {
+        const { rows }: any = await pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING id, name, email', [name, email, result.decoded.id]);
+        if (!rows.length) return res.status(404).json({ error: 'Users not found' });
+        res.json({ data: rows[0] });
+    } catch (error: any) {
+        return res.status(500).json({ error: error.message });
+    }
+});
+
    
 export default router;
