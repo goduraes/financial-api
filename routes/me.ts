26a226ebfc6988beb08cebc4f87bf6bd63c23208
Gabriel Oliveira Duraes <drt.gduraes@ifood3rd.com>
2026-04-16 20:59:37 -0300

    some fixes

--- routes/me.ts
+++ routes/me.ts
@@ -27,7 +27,7 @@ router.put('/edit', authMiddleware(), async (req: Request, res: Response) => {
     
     try {
         const { rows }: any = await pool.query('UPDATE users SET name = $1, email = $2, updated_at = $3 WHERE id = $4 RETURNING *', [name, email, new Date(), token.decoded.id]);
-        if (!rows.length) return res.status(404).json({ error: 'Usuário não encontrado' });
+        if (!rows.length) return res.status(500).json({ error: 'Não foi possível recuperar a linha editada' });
         const { password, ...data} = rows[0];
         res.json({ data: data });
     } catch (error: any) {
