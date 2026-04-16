1088e9d275b7b4a846626b0cfae6fc4ec845f64c
Gabriel Oliveira Duraes <drt.gduraes@ifood3rd.com>
2026-04-16 19:27:02 -0300

    fix me

--- routes/me.ts
+++ routes/me.ts
@@ -26,7 +26,7 @@ router.put('/edit', authMiddleware(), async (req: Request, res: Response) => {
     const { name, email } = req.body;
     
     try {
-        const { rows }: any = await pool.query('UPDATE users SET name = $1, email = $2 updated_at = $3 WHERE id = $4 RETURNING *', [name, email, new Date(), result.decoded.id]);
+        const { rows }: any = await pool.query('UPDATE users SET name = $1, email = $2, updated_at = $3 WHERE id = $4 RETURNING *', [name, email, new Date(), result.decoded.id]);
         if (!rows.length) return res.status(404).json({ error: 'Users not found' });
         const { password, ...data} = rows[0];
         res.json({ data: data });
