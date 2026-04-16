480f5616a3668e8c6b06d6b5235e6e7e661df67d
Gabriel Oliveira Duraes <drt.gduraes@ifood3rd.com>
2026-04-16 19:12:13 -0300

    fix users

--- routes/me.ts
+++ routes/me.ts
@@ -10,9 +10,10 @@ router.get('/', authMiddleware(), async (req: Request, res: Response) => {
     if ("error" in result) return res.status(401).json({ error: result.error });
     
     try {
-        const { rows }: any = await pool.query('SELECT id, name, email, role, created_at, updated_at FROM users WHERE id = $1', [result.decoded.id]);
+        const { rows }: any = await pool.query('SELECT * FROM users WHERE id = $1', [result.decoded.id]);
         if (!rows.length) return res.status(404).json({ error: 'Users not found' });
-        res.json({ data: rows[0] });
+        const { password, ...data} = rows[0];
+        res.json({ data: data });
     } catch (error: any) {
         return res.status(500).json({ error: error.message });
     }
@@ -25,9 +26,10 @@ router.put('/edit', authMiddleware(), async (req: Request, res: Response) => {
     const { name, email } = req.body;
     
     try {
-        const { rows }: any = await pool.query('UPDATE users SET name = $1, email = $2 updated_at = $3 WHERE id = $4 RETURNING id, name, email, created_at, updated_at', [name, email, new Date(), result.decoded.id]);
+        const { rows }: any = await pool.query('UPDATE users SET name = $1, email = $2 updated_at = $3 WHERE id = $4 RETURNING *', [name, email, new Date(), result.decoded.id]);
         if (!rows.length) return res.status(404).json({ error: 'Users not found' });
-        res.json({ data: rows[0] });
+        const { password, ...data} = rows[0];
+        res.json({ data: data });
     } catch (error: any) {
         return res.status(500).json({ error: error.message });
     }
--- routes/users.ts
+++ routes/users.ts
@@ -60,9 +60,10 @@ router.get('/', async (req: Request, res: Response) => {
 // User by id
 router.get('/:id', authMiddleware(true), async (req: Request, res: Response) => {
     try {
-        const { rows }: any = await pool.query('SELECT id, name, email, role, created_at, updated_at FROM users WHERE id = $1', [req.params.id]);
+        const { rows }: any = await pool.query('SELECT * FROM users WHERE id = $1', [req.params.id]);
         if (!rows.length) return res.status(404).json({ error: 'Users not found' });
-        return res.json({ data: rows[0] });
+        const { password, ...data} = rows[0];
+        return res.json({ data: data });
     } catch (error: any) {
         return res.status(500).json({ error: error.message });
     }
@@ -71,9 +72,10 @@ router.get('/:id', authMiddleware(true), async (req: Request, res: Response) => 
 // User by email
 router.get('/:email', authMiddleware(true), async (req: Request, res: Response) => {
     try {
-        const { rows }: any = await pool.query('SELECT id, name, email, role, created_at, updated_at FROM users WHERE email = $1', [req.params.email]);
+        const { rows }: any = await pool.query('SELECT * FROM users WHERE email = $1', [req.params.email]);
         if (!rows.length) return res.status(404).json({ error: 'Users not found' });
-        return res.json({ data: rows[0] });
+        const { password, ...data} = rows[0];
+        return res.json({ data: data });
     } catch (error: any) {
         return res.status(500).json({ error: error.message });
     }
@@ -90,7 +92,8 @@ router.post('/add', async (req: Request, res: Response) => {
         if (user.rows.length) return res.status(409).json({ message: 'E-mail já cadastrado!' });
         const { rows } = await pool.query('INSERT INTO users (name, email, password, created_at) VALUES ($1, $2, $3, $4) RETURNING *', [name, email, hashedPassword, new Date()]);
         if (!rows.length) return res.status(500).json({ error: 'Could not retrieve inserted row id' });
-        return res.status(201).json({ id: rows[0], message: 'Row added successfully' });
+        const { password, ...data} = rows[0];
+        return res.status(201).json({ data: data, message: 'Row added successfully' });
     } catch (error: any) {
         return res.status(500).json({ error: error.message });
     }
@@ -101,7 +104,8 @@ router.delete('/:id', authMiddleware(true), async (req: Request, res: Response) 
     try {
         const { rows }: any = await pool.query('DELETE FROM users WHERE id = $1', [req.params.id]);
         if (!rows.length) return res.status(500).json({ error: 'Could not retrieve affected row count' });
-        return res.json({ message: 'Deleted successfully', rowsAffectedId: rows[0] });
+        const { password, ...data} = rows[0];
+        return res.json({ message: 'Deleted successfully', data: data });
     } catch (error: any) {
         return res.status(500).json({ error: error.message });
     }
