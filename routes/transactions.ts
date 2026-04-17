c683adab22253053e1a008148c4d1d2fe84a4303
Gabriel Oliveira Duraes <drt.gduraes@ifood3rd.com>
2026-04-17 12:36:14 -0300

    fixes

--- routes/transactions.ts
+++ routes/transactions.ts
@@ -11,8 +11,15 @@ router.get('/', authMiddleware(), async (req: Request, res: Response) => {
     if ("error" in token) return res.status(401).json({ error: token.error });
     
     try {
-        const { rows }: any = await pool.query('SELECT * FROM transactions WHERE user_id = $1', [token.decoded.id]);
-        res.json({ data: rows });
+        const { rows }: any = await pool.query(`
+            SELECT 
+                t.*,
+                tag.name AS tag_name
+            FROM transactions t
+            LEFT JOIN tags tag ON tag.id = t.tag_id
+            WHERE t.user_id = $1;    
+        `, [token.decoded.id]);
+        res.json({ data: {...rows} });
     } catch (error: any) {
         return res.status(500).json({ error: error.message });
     }
@@ -25,15 +32,10 @@ router.post('/add', authMiddleware(), async (req: Request, res: Response) => {
     
     const { description, value, date, tag_id, type } = req.body;
 
-    const tag: any = await pool.query('SELECT * FROM tags WHERE id = $1', [tag_id]);
-    if (!tag.rows.length) return res.status(403).json({ error: 'Tag não encontrada' });
-    const tagUserId = tag.rows[0].user_id;
-    if (tagUserId !== token.decoded.id) return res.status(403).json({ error: 'Você não tem permissão' });
-
     try {
         const { rows } = await pool.query('INSERT INTO transactions (description, value, date, tag_id, type, user_id, created_at) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *', [description, value, date, tag_id, type, token.decoded.id, new Date()]);
         if (!rows.length) return res.status(500).json({ error: 'Não foi possível recuperar a linha inserida' });
-        res.json({ data: { ...rows[0], tag_name: tag.rows[0].name } });
+        res.json({ data: rows[0] });
     } catch (error: any) {
         return res.status(500).json({ error: error.message });
     }
