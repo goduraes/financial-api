0b9856526fae95e0ed855734f132f83901c40c0d
Gabriel Oliveira Duraes <drt.gduraes@ifood3rd.com>
2026-04-17 21:18:57 -0300

    add dashboard filter date

--- routes/dashboard.ts
+++ routes/dashboard.ts
@@ -8,6 +8,8 @@ import { extractBearerToken } from '../helpers/extractBearerToken';
 router.get('/summary', authMiddleware(), async (req: Request, res: Response) => {
     const token = extractBearerToken(req.headers.authorization);
     if ("error" in token) return res.status(401).json({ error: token.error });
+
+    const { startDate, endDate } = req.query;
     
     try {
         const { rows }: any = await pool.query(`
@@ -16,8 +18,16 @@ router.get('/summary', authMiddleware(), async (req: Request, res: Response) => 
                 COALESCE(SUM(CASE WHEN type = 'EXPENSE' THEN value ELSE 0 END), 0)::FLOAT AS total_expense,
                 COALESCE(SUM(CASE WHEN type = 'INCOME' THEN value ELSE -value END), 0)::FLOAT AS balance
             FROM transactions
-            WHERE user_id = $1
-        `, [token.decoded.id]);
+            WHERE user_id = $3
+            AND (
+                $1::date IS NULL
+                OR date >= $1::date
+            )
+            AND (
+                $2::date IS NULL
+                OR date < ($2::date + INTERVAL '1 day')
+            );
+        `, [startDate, endDate, token.decoded.id]);
 
         if (!rows.length) return res.status(500).json({ error: 'Erro ao buscar dados' });
         
